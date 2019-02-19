be326d64c4d6c092480040dcc9de66a9482ff20b
Leandro Pinheiro de Holanda <leandro.p.holanda@eisa.corp.com>
2019-02-19 10:17:16 -0300

    first commit

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,78 @@
+const LivroDAO = require('../infra/livro-dao');
+const db = require('../../config/database');
+
+module.exports = (app) => {
+    app.get('/', function(req, resp) {
+        resp.send(
+            `
+            <html>
+                <head>
+                    <meta charset="utf-8"></meta>
+                </head>
+                <body>
+                    <h1> Casa do Código</h1>
+                </body>
+            </html>    
+            `
+        );
+    });
+    
+    app.get('/livros', function(req, resp) {
+
+        const livroDAO = new LivroDAO(db);
+        livroDAO.lista()
+                .then(livros => resp.marko(
+                    require('../views/livros/lista/lista.marko'),
+                    {
+                        livros: livros
+                    }
+                ))
+                .catch(erro => console.log(erro));
+    })
+
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'), {livro: {} });
+    }); 
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDAO(db);
+
+        livroDao.buscaPorId(id)
+                .then(livro =>
+                    resp.marko(
+                        require('../views/livros/form/form.marko'),
+                        { livro: livro}
+                    )
+                ).catch(erro => console.log(erro));
+    })
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+
+        const livroDAO = new LivroDAO(db);
+
+        livroDAO.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+
+        const livroDAO = new LivroDAO(db);
+
+        livroDAO.atualiza(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+
+        const livroDao = new livroDAO(db);
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
+    })
+};
